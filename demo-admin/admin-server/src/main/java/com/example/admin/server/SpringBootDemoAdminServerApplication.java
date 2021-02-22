431f9230d5497f914779621b677e2aff88d88805
ghostSamu <173251317@qq.com>
2021-02-22 16:25:00 +0800

    demo-admin

---
+++ demo-admin/admin-server/src/main/java/com/example/admin/server/SpringBootDemoAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootDemoAdminServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootDemoAdminServerApplication.class,args);
+    }
+}
